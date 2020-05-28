f05b400ff407c00c93f9365afa6026788d943695
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2020-05-27 21:42:18 -0400

    Improving Visual Feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,6 +15,7 @@ class ContactData extends Component {
         value: '',
         validation: { required: true },
         valid: false,
+        touched: false,
       },
       street: {
         elementType: 'input',
@@ -22,6 +23,7 @@ class ContactData extends Component {
         value: '',
         validation: { required: true },
         valid: false,
+        touched: false,
       },
       zipCode: {
         elementType: 'input',
@@ -29,6 +31,7 @@ class ContactData extends Component {
         value: '',
         validation: { required: true, minLength: 5, maxLenght: 5 },
         valid: false,
+        touched: false,
       },
       country: {
         elementType: 'input',
@@ -36,6 +39,7 @@ class ContactData extends Component {
         value: '',
         validation: { required: true },
         valid: false,
+        touched: false,
       },
       email: {
         elementType: 'email',
@@ -43,6 +47,7 @@ class ContactData extends Component {
         value: '',
         validation: { required: true },
         valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -103,6 +108,7 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation,
     );
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
@@ -123,6 +129,7 @@ class ContactData extends Component {
             value={formElement.config.value}
             invalid={!formElement.config.valid}
             shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
